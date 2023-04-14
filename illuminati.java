984a5dde921586acf2162904af1fed07f94ac399
honmono9999 <b213213@naver.com>
2023-04-14 15:10:20 +0900

    ▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄ █░░░░░░░░▀█▄▀▄▀██████░▀█▄▀▄▀██████░ ░░░░░░░░░░░▀█▄█▄███▀░░░ ▀██▄█▄███▀░

---
+++ illuminati.java
@@ -0,0 +1,11 @@
+public class illuminati {
+    public static void  illuminati(char args,byte A) {
+        for (byte i=A;i>=0;i--){
+
+            for (int ait=i;ait>0;ait--){
+                System.out.print(" ");
+            }
+            System.out.println(args);
+        }
+    }
+}
--- 짭바.java
+++ 짭바.java
@@ -1,1 +1,8 @@
-//awesome
+public class 짭바 {
+
+    illuminati a;
+    public static void main(String[] args) {
+        System.out.println("");
+    }
+
+}
